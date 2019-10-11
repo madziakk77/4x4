c59f1d567c30cc433d0be8873f32749b47d9b547
madziakk77 <42512679+madziakk77@users.noreply.github.com>
2019-10-11 07:13:27 +0000

    Create pop4x4.py
    Beginning of new approach to check out all possible sequences.
    Goal is: 6 numbers plus 6 numbers, like x1, x2, x3, x4, x5, x6, and x7, x8, x9, x10, x11, x12 with a correlation between.
    Starts from 8 numbers x1... x8.
    1. rule: in x1...x6 must be 4 from last and around.
    2. no 5 the same as ever (since at last few years)
    ...
    
    Hits in lottery on site: lotto.pl (official polish site of national lottery)
    All hit in lottery... you must find yourself ;)

---
+++ pop4x4.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+row_all = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+#row_all = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49]
+
+all00 = 0
+
+plik = open("pop4x4.txt", "wb")
+for a1 in row_all[0:41]:
+ print a1
+ for a2 in row_all[1:42]:
+  for a3 in row_all[2:43]:
+   for a4 in row_all[3:44]:
+    for a5 in row_all[4:45]:
+     for a6 in row_all[5:46]:
+      for a7 in row_all[6:47]:
+       for a8 in row_all[7:48]:
+        ttt = [a1, a2, a3, a4, a5, a6, a7, a8]
+        all00 = all00 + 1
+#        plik.write("\n" + str(ttt)
+plik.close()
+print all00
